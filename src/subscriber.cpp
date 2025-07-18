0eab31bfbc5d3d9844f6bc2331345c7ce95f4485
jung <jeongmy@uci.edu>
2025-07-18 01:49:33 -0700

    Visualize the RGB images on RVIZ

--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -1,22 +1,67 @@
 #include <memory>
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <functional>
+
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "rclcpp/serialization.hpp"
+#include "rosbag2_transport/reader_writer_factory.hpp"
+#include "rosbag2_storage/storage_options.hpp"
+#include "sensor_msgs/msg/image.hpp"
+using namespace std::chrono_literals;
 
 class KittiSubscriber : public rclcpp::Node
 {
     public:
-        KittiSubscriber() : Node("kitti_subscriber")
+        KittiSubscriber(const std::string & bag_filename) : Node("kitti_subscriber")
         {
-            
+            publisher_left_raw_image_ = this->create_publisher<sensor_msgs::msg::Image>("/kitti/camera_color_left/image_raw", 10);
+            timer_ = this->create_wall_timer(
+                100ms, std::bind(&KittiSubscriber::timer_callback, this)
+            );
+            rosbag2_storage::StorageOptions storage_options;
+            storage_options.uri = bag_filename;
+            storage_options.storage_id = "sqlite3";
+
+            reader_ = rosbag2_transport::ReaderWriterFactory::make_reader(storage_options); 
+            reader_ -> open(storage_options);
+
+            RCLCPP_INFO(this->get_logger(), "Opened bag file and staring image show: %s", bag_filename.c_str());
         }
     private:
-        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+        void timer_callback()
+        {   
+            //stopping condition: reached the end of bag file.
+            if(!reader_ -> has_next())
+            {
+                rclcpp::shutdown();
+                return;
+            }
+            rosbag2_storage::SerializedBagMessageSharedPtr msg = reader_ -> read_next();
+            
+            if (msg->topic_name == "/kitti/camera_color_left/image_raw")
+            {
+                rclcpp::SerializedMessage serialized_msg(*msg -> serialized_data);
+                sensor_msgs::msg::Image::SharedPtr ros_msg = std::make_shared<sensor_msgs::msg::Image>();
+                serialization_.deserialize_message(&serialized_msg, ros_msg.get());
+                publisher_left_raw_image_->publish(*ros_msg);
+
+
+            }
+        }
+        rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_left_raw_image_;
+        rclcpp::TimerBase::SharedPtr timer_;
+        rclcpp::Serialization<sensor_msgs::msg::Image> serialization_;
+        std::unique_ptr<rosbag2_cpp::Reader> reader_;
 };
 
-int main()
+int main(int argc, char ** argv)
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<KittiSubscriber>());
+    auto node = std::make_shared<KittiSubscriber>(argv[1]);
+    rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 }
