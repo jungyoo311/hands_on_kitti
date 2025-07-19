e5704b4d8b853d24ab64733dfcc8cdbe105cf19a
jung <jeongmy@uci.edu>
2025-07-19 01:07:28 -0700

    point cloud subscription modified

--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -10,6 +10,7 @@
 #include "rosbag2_transport/reader_writer_factory.hpp"
 #include "rosbag2_storage/storage_options.hpp"
 #include "sensor_msgs/msg/image.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp" // Don't forget to add 
 using namespace std::chrono_literals;
 
 class KittiSubscriber : public rclcpp::Node
@@ -18,12 +19,13 @@ class KittiSubscriber : public rclcpp::Node
         KittiSubscriber(const std::string & bag_filename) : Node("kitti_subscriber")
         {
             publisher_left_raw_image_ = this->create_publisher<sensor_msgs::msg::Image>("/kitti/camera_color_left/image_raw", 10);
+            publisher_point_cloud_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/kitti/velo/pointcloud", 10);
             timer_ = this->create_wall_timer(
                 100ms, std::bind(&KittiSubscriber::timer_callback, this)
             );
             rosbag2_storage::StorageOptions storage_options;
             storage_options.uri = bag_filename;
-            //storage_options.storage_id = "sqlite3"; // no need i think
+            storage_options.storage_id = "sqlite3";
 
             reader_ = rosbag2_transport::ReaderWriterFactory::make_reader(storage_options); 
             reader_ -> open(storage_options);
@@ -32,7 +34,7 @@ class KittiSubscriber : public rclcpp::Node
         }
     private:
         void timer_callback()
-        {   
+        {
             //stopping condition: reached the end of bag file.
             if(!reader_ -> has_next())
             {
@@ -44,16 +46,27 @@ class KittiSubscriber : public rclcpp::Node
             if (msg->topic_name == "/kitti/camera_color_left/image_raw")
             {
                 rclcpp::SerializedMessage serialized_msg(*msg -> serialized_data);
-                sensor_msgs::msg::Image::SharedPtr ros_msg = std::make_shared<sensor_msgs::msg::Image>();
-                serialization_.deserialize_message(&serialized_msg, ros_msg.get());
-                publisher_left_raw_image_->publish(*ros_msg);
+                sensor_msgs::msg::Image::SharedPtr ros_msg_cam1 = std::make_shared<sensor_msgs::msg::Image>();
+                serialization_cam1_.deserialize_message(&serialized_msg, ros_msg_cam1.get());
+                publisher_left_raw_image_->publish(*ros_msg_cam1);
             }
             // ToDo: Visualize Lidar point cloud on RVIZ
+            if (msg->topic_name == "/kitti/velo/pointcloud")
+            {
+                rclcpp::SerializedMessage serialized_msg(*msg ->serialized_data);
+                sensor_msgs::msg::PointCloud2::SharedPtr ros_msg_pointcloud = std::make_shared<sensor_msgs::msg::PointCloud2>();
+                serialization_pointcloud_.deserialize_message(&serialized_msg, ros_msg_pointcloud.get());
+                publisher_point_cloud_->publish(*ros_msg_pointcloud);
+            }
         }
         rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_left_raw_image_;
+        rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_point_cloud_;
         rclcpp::TimerBase::SharedPtr timer_;
-        rclcpp::Serialization<sensor_msgs::msg::Image> serialization_;
+        rclcpp::Serialization<sensor_msgs::msg::Image> serialization_cam1_;
+        rclcpp::Serialization<sensor_msgs::msg::PointCloud2>  serialization_pointcloud_;
         std::unique_ptr<rosbag2_cpp::Reader> reader_; // why we choose unique ptr here?:
+        // ans: use unique_ptr or shared_ptr to avoid forgetting to delete objects created using new
+        
 };
 
 int main(int argc, char ** argv)
