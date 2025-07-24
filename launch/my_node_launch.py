6eee646295bc67c8ee0a7bbf143ebef5674bb012
jung <jeongmy@uci.edu>
2025-07-24 12:13:28 -0700

    launch file added

---
+++ launch/my_node_launch.py
@@ -0,0 +1,13 @@
+# launch a single launch file and play bagfile
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Node(
+        #     package=''
+        #     namespace=''
+        #     executable=''
+        #     name=''
+        # )
+    ])
