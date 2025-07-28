4aecd0be37cfab75fc191741388e160b013b7135
jung <jeongmy@uci.edu>
2025-07-27 21:13:36 -0700

    branch clean

--- build/hands_on_kitti/CMakeFiles/hands_on_kitti_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/hands_on_kitti_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hands_on_kitti_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/hands_on_kitti/CMakeFiles/hands_on_kitti_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for hands_on_kitti_uninstall.
-# This may be replaced when dependencies are built.
